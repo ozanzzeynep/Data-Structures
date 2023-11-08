ab99311625940a79dab8697dfa4a7842104aac0c
ZeynepOzan <ozanzzeynep@gmail.com>
2023-11-08 15:26:26 +0300

    exercise1 added

---
+++ src/main/java/leetcode_exercises/Main.java
@@ -0,0 +1,17 @@
+package leetcode_exercises;
+
+public class Main {
+    public static void main(String[] args) {
+        FindMiddleNode myList = new FindMiddleNode(1);
+        myList.append(2);
+        myList.append(3);
+        myList.append(4);
+        myList.append(5);
+
+
+        System.out.println("----------");
+        myList.printAll();
+        System.out.println("----------");
+        System.out.println(myList.findMiddleNode().value);
+    }
+}
