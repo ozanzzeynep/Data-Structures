36dccc6de6f24685129b7b1a40f22a5106c2ccbc
ZeynepOzan <ozanzzeynep@gmail.com>
2023-11-08 16:42:41 +0300

    141.Linked List Cycle algorithm solved

--- src/main/java/leetcode_exercises/Main.java
+++ src/main/java/leetcode_exercises/Main.java
@@ -9,10 +9,25 @@ public class Main {
         myList.append(5);
 
 
-
         System.out.println("----------");
         myList.printAll();
         System.out.println("----------");
         System.out.println(myList.findMiddleNode().value);
+
+
+        System.out.println("-------------");
+
+        HasLoop myList2 = new HasLoop(3);
+        myList2.append(2);
+        myList2.append(0);
+        myList2.append(-4);
+        myList2.append(5);
+        myList2.append(8);
+
+
+        myList2.printAll();
+        System.out.println(myList2.hasLoop());
+
+        System.out.println("-------------------");
     }
 }
