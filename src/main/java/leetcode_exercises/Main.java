fe81cebf8f33d3b5c7bb50060b927c7ddc8747b0
ZeynepOzan <ozanzzeynep@gmail.com>
2023-11-08 17:16:42 +0300

    findKthNodeFromEnd algorithm exercises added

--- src/main/java/leetcode_exercises/Main.java
+++ src/main/java/leetcode_exercises/Main.java
@@ -2,6 +2,8 @@ package leetcode_exercises;
 
 public class Main {
     public static void main(String[] args) {
+
+        //FindMiddleNode
         FindMiddleNode myList = new FindMiddleNode(1);
         myList.append(2);
         myList.append(3);
@@ -17,6 +19,8 @@ public class Main {
 
         System.out.println("-------------");
 
+        //HassLoop
+
         HasLoop myList2 = new HasLoop(3);
         myList2.append(2);
         myList2.append(0);
@@ -29,5 +33,16 @@ public class Main {
         System.out.println(myList2.hasLoop());
 
         System.out.println("-------------------");
+
+        //FindKthNodeFromEnd
+
+        FindKthNodeFromEnd findMyList = new FindKthNodeFromEnd(1);
+        findMyList.append(2);
+        findMyList.append(3);
+        findMyList.append(4);
+        findMyList.append(5);
+
+        System.out.println(findMyList.findKthFromEnd(2).value);
+
     }
 }
